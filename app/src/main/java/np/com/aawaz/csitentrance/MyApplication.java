b1847897a5b1be2b0aa9eedbc9b85b23b0cf1d7c
Aawaz Gyawali <awazgyawali@gmail.com>
2015-07-20 23:47:37 +0545

    Volley Singleton

---
+++ app/src/main/java/np/com/aawaz/csitentrance/MyApplication.java
@@ -0,0 +1,25 @@
+package np.com.aawaz.csitentrance;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    private static MyApplication sInstance;
+
+    public static MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public static Context getAppContext() {
+        return sInstance.getApplicationContext();
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+    }
+
+
+}
