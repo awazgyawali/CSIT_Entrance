f9f6f652b3d1c8e58ba212d969edb2b100a67646
Aawaz Gyawali <awazgyawali@gmail.com>
2015-07-29 10:01:55 +0545

    Minor bugs fixed

--- app/src/main/java/np/com/aawaz/csitentrance/AdvanceClasses/MyApplication.java
+++ app/src/main/java/np/com/aawaz/csitentrance/AdvanceClasses/MyApplication.java
@@ -3,6 +3,20 @@ package np.com.aawaz.csitentrance.AdvanceClasses;
 import android.app.Application;
 import android.content.Context;
 
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import np.com.aawaz.csitentrance.R;
+
+@ReportsCrashes(mailTo = "info@aawaz.com.np",
+        mode = ReportingInteractionMode.DIALOG,// optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = android.R.drawable.ic_dialog_info,
+        resDialogTitle = R.string.crash_dialog_title,
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. When defined, adds a user email text entry with this text resource as label. The email address will be populated from SharedPreferences and will be provided as an ACRA field if configured.
+        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+)
 public class MyApplication extends Application {
     private static MyApplication sInstance;
 
@@ -19,6 +33,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sInstance = this;
+        ACRA.init(this);
     }
 
 
