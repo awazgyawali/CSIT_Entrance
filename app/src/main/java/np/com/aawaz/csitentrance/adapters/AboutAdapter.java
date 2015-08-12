049a5a650b2e522df0132a0b0874ff3f44f0f192
Aawaz Gyawali <awazgyawali@gmail.com>
2015-08-12 20:16:25 +0545

    Everything normal except the fab. Fixing soon.

--- app/src/main/java/np/com/aawaz/csitentrance/adapters/AboutAdapter.java
+++ app/src/main/java/np/com/aawaz/csitentrance/adapters/AboutAdapter.java
@@ -30,12 +30,12 @@ public class AboutAdapter extends RecyclerView.Adapter<AboutAdapter.ViewHolder> 
                 "Buddhiraj Nagarkoti\nGraphics Designer", "Prasanna Mishra\nUI Designer",
                 "Gopal Nepal","Bishal Rana Magar","Ksitiz Khanal"};
         twitterHandle=new String[]{"AawazGyawali","RameshworDhakal","","Prascq56",
-                "","",""};
-        twiterUserId=new String[]{"2960523083","3080519719","","2475260587"};
-        fbLink=new String[]{"https://www.facebook.com/muskangyawali","https://www.facebook.com/ranjit.sharma.39904",
-                "https://www.facebook.com/Fcukyna","https://www.facebook.com/prasanna.mishra.79",
-                "https://www.facebook.com/gopal.nepal.376","https://www.facebook.com/bishalr",
-                "https://www.facebook.com/kshitij.khanal.5"};
+                "Meet2Gopal", "meet2_bishal", ""};
+        twiterUserId = new String[]{"2960523083", "3080519719", "", "2475260587", "953087550", "2921352510", ""};
+        fbLink = new String[]{"muskangyawali", "ranjit.sharma.39904",
+                "Fcukyna", "prasanna.mishra.79",
+                "gopal.nepal.376", "bishalr",
+                "kshitij.khanal.5"};
         fbId=new String[]{"100000234287319","100004035110394","100001168909448","100000242453492","100003176412331",
                 "100000360023008","100004016767543"};
     }
@@ -78,7 +78,7 @@ public class AboutAdapter extends RecyclerView.Adapter<AboutAdapter.ViewHolder> 
                         context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
                         i = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/" + fbId[position]));
                     } catch (Exception e) {
-                        i = new Intent(Intent.ACTION_VIEW, Uri.parse(fbLink[position]));
+                        i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/" + fbLink[position]));
                     }
                     try {
                         context.startActivity(i);
